33226bc0278897e66fe23594042bcf1ed5c26bab
FouadHijazi <fouad.o.hijazi@gmail.com>
2022-05-12 03:14:49 +0300

    Q01-E-Math

---
+++ Round1/Q01-E-Math/main.js
@@ -0,0 +1,30 @@
+console.log(arrowTitle);
+
+/* Even or Not */
+
+/*  
+Write a function that returns true if a number is even, and false otherwise.
+*/
+
+//Algurethim & Sodo Code
+//we need to write a function with one parameters number to return true or false
+//If the will be even retun to us true || if the number not even return false
+
+const evenOrNot = (num) => {
+  if(num % 2 === 0){
+    return true
+  }
+  return false
+};
+
+/*
+Examples:
+evenOrNot(1) // => false
+evenOrNot(2) // => true
+evenOrNot(5) // => false
+evenOrNot(99) // => false
+evenOrNot(0) // => true
+evenOrNot(-8) // => true
+evenOrNot(-17) // => false
+*/
+module.exports = {evenOrNot} ;
