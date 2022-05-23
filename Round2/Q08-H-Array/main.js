b71f9c854e9aad69d39fcc9bca4d6fd22df97b4e
FouadHijazi <fouad.o.hijazi@gmail.com>
2022-05-23 23:54:45 +0300

    Update

--- Round2/Q08-H-Array/main.js
+++ Round2/Q08-H-Array/main.js
@@ -6,8 +6,26 @@ console.log(arrowTitle);
 Write a function calculates the Highest possible sum of any n consecutive numbers in an array arr of integers (where n ≥ 1).
 */
 
+//sodoCode
+//first we will try to find the biggest number in the array
+//we will start with an initial value 0
+//here we will sum the values of the length num then assign it to maxsum
+//>> we will iterate the main array taking into consideration the num array and then take the max value
+//>> we will start iterate the array start from num not from zero to check the next elements after the num array length
+//>> will check the sum of the array after num length array
+//At the end will compare between two sums to took the maximum sum
+
 const findHighestPossibleSum = (arr, num) => {
-  // YOUR CODE HERE
+  let maxsum = 0;
+  for (let i = 0; i < num; i++) {
+    maxsum = maxsum + arr[i];
+  }
+  let tempSum = maxsum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum + arr[i] - arr[i - num];
+    maxsum = Math.max(tempSum, maxsum);
+  }
+  return maxsum;
 };
 
 /*
@@ -31,4 +49,3 @@ findHighestPossibleSum([10, 2, 10, 9, 3, 10, 4, -7, 2, 6, 8], 3) // => 22
 findHighestPossibleSum([5, 2, 10, 9, 3, 10, 4, -7, 2, 6, 8], 4) // => 32
 findHighestPossibleSum([5, 2, 10, 9, 3, 10, 4, -7, 2, 6, 8], 5) // => 36
 */
-
