e0ba040dd8c3711cea517a6f53badcd6bc7e8b89
FouadHijazi <fouad.o.hijazi@gmail.com>
2022-06-10 03:07:12 +0300

    Solv

--- Round3/Q11-H-String/main.js
+++ Round3/Q11-H-String/main.js
@@ -8,6 +8,29 @@ have a string, delete any characters that are not unique from the string.
 
 const removeNotUnique = (str) => {
   // YOUR CODE HERE
+  const removeNotUnique = (str) => {
+    if (!str.length) {
+      return "it's empty";
+    }
+    let strArr = str.split("");
+    for (let i = 0; i < strArr.length; i++) {
+      for (let j = 0; j < strArr.length; j++) {
+        if (strArr[i] === " ") {
+          strArr.splice(i, 1);
+          i--;
+          continue;
+        }
+        if (strArr[i] === strArr[j] && i !== j) {
+          strArr.splice(i, 1);
+          j--;
+          strArr.splice(j, 1);
+          i--;
+          j--;
+        }
+      }
+    }
+    return strArr.join("");
+  };
 };
 
 /* 
