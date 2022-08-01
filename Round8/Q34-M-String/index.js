1ef92ff0e0cab43e49fce33e89088d417cb6edc7
FouadHijazi <fouad.o.hijazi@gmail.com>
2022-08-01 03:52:45 +0300

    Q34

--- Round8/Q34-M-String/index.js
+++ Round8/Q34-M-String/index.js
@@ -8,7 +8,27 @@
 */
 
 const maxChar = (str) => {
-  // TODO: your code here
-};
+     let obj = {}
+     let counter = 0
+     let result = ""
+
+     for (let i = 0; i < str.length; i++) {
+          obj[str[i]] = obj[str[i]] ? obj[str[i]] + 1 : 1
+       
+     }
+   
+     for (const key in obj) {
+         if (obj[key] > counter){
+          counter = obj[key] 
+          result = key
+         }
+         return result
+     }
+
+     /* let result = Object.keys(obj).reduce(function(a,b){return obj[a] > obj[b] ? a : b})
+     
+     return result */
+}
+console.log(maxChar("abcccccccdddd"));
 
 module.exports = maxChar;
